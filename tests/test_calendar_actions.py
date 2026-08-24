bbd4d45259b23433575e0c9d9b5c9cef0cae061f
addn2030-svg <addn2030@gmail.com>
2026-08-25 00:38:02 +0300

    Test Arabic Calendar request parsing

---
+++ tests/test_calendar_actions.py
@@ -0,0 +1,35 @@
+import datetime as dt
+import unittest
+from zoneinfo import ZoneInfo
+
+from connectors.calendar_actions import parse_event_request
+
+TZ = ZoneInfo("Asia/Riyadh")
+
+
+class CalendarActionsTests(unittest.TestCase):
+    def setUp(self):
+        self.base = dt.datetime(2026, 8, 25, 10, 0, tzinfo=TZ)
+
+    def test_arabic_tomorrow_evening(self):
+        event = parse_event_request("ذكرني غدًا الساعة 5:30 مساءً بالتمرين قبل ساعتين", self.base)
+        self.assertEqual(event["start"], dt.datetime(2026, 8, 26, 17, 30, tzinfo=TZ))
+        self.assertEqual(event["reminder_minutes"], 120)
+        self.assertIn("بالتمرين", event["title"])
+
+    def test_iso_date_and_am(self):
+        event = parse_event_request("/remind اجتماع العمير 2026-08-27 الساعة 8 صباحًا", self.base)
+        self.assertEqual(event["start"].hour, 8)
+        self.assertEqual(event["start"].date(), dt.date(2026, 8, 27))
+
+    def test_requires_date(self):
+        with self.assertRaises(ValueError):
+            parse_event_request("ذكرني الساعة 5 مساءً", self.base)
+
+    def test_duration(self):
+        event = parse_event_request("أضف مراجعة غدًا الساعة 4 مساءً لمدة 90 دقيقة", self.base)
+        self.assertEqual(int((event["end"] - event["start"]).total_seconds() / 60), 90)
+
+
+if __name__ == "__main__":
+    unittest.main()
