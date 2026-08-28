4ce3db5f2c13f7263339fd95287c77e9340f9a10
addn2030-svg <addn2030@gmail.com>
2026-08-28 13:22:07 +0300

    Install provider diagnostics overlay

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -2,5 +2,7 @@
 
 from . import model_gateway as model_gateway
 from .direct_specialists import install as _install_direct_specialists
+from .provider_diagnostics import install as _install_provider_diagnostics
 
 _install_direct_specialists(model_gateway)
+_install_provider_diagnostics(model_gateway)
